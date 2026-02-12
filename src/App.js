f7a94d2a7e4315dbc0cf81b8ca171058d4215603
kamal kafi <kamalkafi12@gmail.com>
2026-02-12 06:14:07 +0200

    feat: Add logo and enhance task management UI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Header } from "./components/Header";
 import { AddTask } from "./components/AddTask";
 import { ShowTask } from "./components/ShowTask";
 
+import './App.css';
+
 const App = () => {
   return (
     <div className="App">
